9f767af55a4547ce6a974d2319f7a3545295a14e
Brandon Williams <bwei.williams@gmail.com>
2017-08-12 00:57:33 -0600

    lyric search by query

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,36 +1,54 @@
+const Discord = require('discord.js');
 const getArtistTitle = require('get-artist-title');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const settings = require('../settings.json');
 
-// async scrapeLyrics(url) {
-//   const response = await fetch(url);
-//   const text = await response.text();
-//   const $ = cheerio.load(text)
-// }
+const baseURL = `https://api.genius.com/search?access_token=${settings.genius}`;
+let playlist;
+
+const scrapeLyrics = path => {
+  return axios.get(path)
+    .then(response => {
+      let $ = cheerio.load(response.data);
+      return [$('.header_with_cover_art-primary_info-title').text().trim(), $('.lyrics').text().trim()];
+    })
+    .catch(err => {
+      console.warn(`Error fetching lyrics: ${err}`);
+    });
+}
+
+const checkSpotify = hits => {
+  return hits[0].result.primary_artist.name === 'Spotify' ? hits[1].result.url : hits[0].result.url;
+}
+
+const searchLyrics = url => {
+  return Promise.resolve(axios.get(url, {'Authorization': `Bearer ${settings.genius}`})
+    .then(response => path = checkSpotify(response.data.response.hits))
+    .then(path => scrapeLyrics(path))
+    .catch(err => {
+      message.channel.send(`No lyrics found for: ${query} 🙁`, {code:'asciidoc'});
+      console.warn(err);
+    })
+  )
+}
 
 exports.run = function(client, message, args) {
-  const playlist = client.playlist;
+  playlist = client.playlist;
 
   if (!args[0]) return message.channel.send('Please supply an option');
-  if (args[0] === 'np') {
-    // lyrics for current song
-    let [artist, title] = getArtistTitle(playlist.nowPlaying);
-    let url = `https://api.genius.com/search?access_token=${settings.genius}&q=${title}%20${artist}`;
-    axios.get(url).then(response => {
-      if (response.data.response.hits.length) {
-        let path = response.data.response.hits[0].result.path;
-        axios.get(`https://genius.com${path}`).then(response => {
-          let $ = cheerio.load(response.data);
-          let lyrics = $('.lyrics').text().trim();
-          message.channel.send(`Lyrics for: ${playlist.nowPlaying}\n\n${lyrics}`, {code:'asciidoc'});
-        });
-      }
-     });
-  } else {
-    // search query
 
-  }
+  let query, url;
+  args[0] === 'np' ? query = getArtistTitle(playlist.nowPlaying).join(' ') : query = args.slice(1).join(' ');
+  
+  searchLyrics(`${baseURL}&q=${encodeURIComponent(query)}`)
+    .then(songData => {
+      const embed = new Discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setTitle(`Lyrics for: ${songData[0]}`)
+        .setDescription(songData[1])
+        return message.channel.send({embed});
+    });
 }
 
 exports.help = {
