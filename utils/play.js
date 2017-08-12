e02e964ad32204d138030302c9590abada7c6607
Brandon Williams <bwei.williams@gmail.com>
2017-08-11 21:49:16 -0600

    title is stored with song in queue

--- utils/play.js
+++ utils/play.js
@@ -2,19 +2,16 @@ const YTDL = require('ytdl-core');
 
 function play(client, message) {
   const playlist = message.client.playlist;
-  const stream = YTDL(playlist.queue[0], {filter: 'audioonly'});
+  const stream = YTDL(playlist.queue[0].link, {filter: 'audioonly'});
   playlist.dispatcher = client.voiceConnection.playStream(stream);
-
-  message.channel.send(`Now playing: ${playlist.queue[0]}`, {code:'asciidoc'});
-
+  playlist.nowPlaying = playlist.queue[0].title;
+  // message.channel.send(`Now playing: ${playlist.queue[0]}`, {code:'asciidoc'});
   playlist.queue.shift();
 
   playlist.dispatcher.on('end', () => {
     if (playlist.queue.length) {
       // wait half a second to prevent dispatcher race condition
       setTimeout( () => { play(client.voiceConnection, message) }, 500);
-    } else {
-      message.channel.send('No songs in queue', {code:'asciidoc'});
     }
   });
 }
